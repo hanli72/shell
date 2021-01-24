9f65e346bfb1c0b276fb268d5aecf10ff9df11f6
Tony <hanli712@163.com>
2021-01-24 13:30:40 +0800

    modify ssd_per_temp.sh for time device and logfilename setting

--- ssd_perfo_temp.sh
+++ ssd_perfo_temp.sh
@@ -6,17 +6,32 @@
 #	> Created Time: 2021年01月23日 星期六 23时01分42秒
 # ************************************************************************/
 
+intervaltime=$1
+devicename=$2
+logfilename=$3
 
-# $1 is for interval time setting.
-# $2 is for SSD device name /dev/xxx
-# $3 is for file name setting
-filename=$2
+if [[ -z $1 ]];then
+    echo please set interval time [1-10]s
+    read intervaltime
+    intervaltime=$intervaltime
+fi
+if [[ -z $2  ]];then
+    echo please set device Name
+    read devicename
+    devicename=${devicename}
+fi
+if [[ -z $3  ]];then
+    echo please set log File Name
+    read logname
+    logfilename=${logname}.txt
+fi
+
 source ./shell_function/time.sh
 start_time
 
 while(true)
 do
 #interleave time setting
-	sleep $1
+	sleep $intervaltime
     runningtime
 done
