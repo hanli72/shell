6f512a40c984ffd642a3c4e94d2a484c9ffeb11a
hanli712 <hanli712@163.com>
2021-01-25 08:34:55 +0800

    update adding_totoal.sh

--- adding_total.sh
+++ adding_total.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd ./log/
-cat pay.txt |awk 'NR==1 {printf "%10s %10s %10s %10s %10s \n", $1, $2,$3,$4, "total"}; NR>=2 {total=$2+$3+$4; printf "%10s %10d %10d %10d %10.2f \n", $1, $2,$3,$4,total}'
+#cat pay.txt |awk 'NR==1 {printf "%10s %10s %10s %10s %10s \n", $1, $2,$3,$4, "total"}; NR>=2 {total=$2+$3+$4; printf "%10s %10d %10d %10d %10.2f \n", $1, $2,$3,$4,total}'
+#below command uses "enter" instead of ";", which has same function has above
+cat pay.txt |awk 'NR==1 {printf "%10s %10s %10s %10s %10s \n", $1, $2,$3,$4, "total"}
+NR>=2 {total=$2+$3+$4
+printf "%10s %10d %10d %10d %10.2f \n", $1, $2,$3,$4,total}'
